cac71d1dfae32c4f68afd0cf091f10483114eb07
Kenneth Karunia <kennethkarunia@Kenneths-MacBook-Pro-2.local>
2019-08-17 19:20:41 +0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,13 +1,26 @@
 var cards=["queen","queen","king","king"];
 var cardsInPlay=[];
-var cardOne = cards[0];
-var cardTwo = cards[1];
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
 
-if (cardsInPlay.length===2){
+
+function checkForMatch(){
+	if (cardsInPlay[0] === cardsInPlay[2]) {
+  console.log("You found a match!");
+} else {
+  console.log("Sorry, try again.");
+}
+}
+
+/*var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+console.log("User flipped " + cardOne);
+
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+console.log("User flipped " + cardTwo);
+*/
+function flipCard(cardId){
+console.log("User flipped " + cards[cardId]);
+/*	if (cardsInPlay.length===2){
 	if (cardsInPlay[0]===cardsInPlay[1]){
 		alert("You Found a match!");
 	} else{
@@ -16,3 +29,10 @@ if (cardsInPlay.length===2){
 }else{
 	console.log("shit")
 }
+*/
+}
+cardsInPlay.push(cards);
+cardsInPlay.push(cards);
+checkForMatch();
+flipCard(0);
+flipCard(2);
