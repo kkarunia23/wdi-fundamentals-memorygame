4619d565175ce2ccd8b2d690db26e2180a7971fb
Kenneth Karunia <kennethkarunia@Kenneths-MacBook-Pro-2.local>
2019-08-17 17:17:38 +0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,8 +1,18 @@
-console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-
+var cards=["queen","queen","king","king"];
+var cardsInPlay=[];
+var cardOne = cards[0];
+var cardTwo = cards[1];
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
 console.log("User flipped " + cardOne);
 console.log("User flipped " + cardTwo);
+
+if (cardsInPlay.length===2){
+	if (cardsInPlay[0]===cardsInPlay[1]){
+		alert("You Found a match!");
+	} else{
+		alert("Sorry, try again");
+	}
+}else{
+	console.log("shit")
+}
